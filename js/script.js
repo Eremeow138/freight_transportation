b653b36534131bbc1bfadb332d4bfdbd8a0aec9d
Eremeow138 <60056105+Eremeow138@users.noreply.github.com>
2020-07-04 16:47:50 +0800

    Новые иконочные шрифты + 2 экран + слайдер v1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$('.js-slider').slick({
+    // setting-name: setting-value
+    nextArrow: '<div class="slick-arrowBox slick-arrowBox_top"><button type="button" class="slick-next slick-btn">След</button><i class="icon-arrow-right"></i></div>',
+    prevArrow: '<div class="slick-arrowBox slick-arrowBox_bottom"><i class="icon-arrow-left"></i><button type="button" class="slick-prev slick-btn">Назад</button></div>',
+
+    speed: 700,
+    fade: true,
+  });
